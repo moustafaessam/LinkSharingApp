5836efa58bf1ad4e0e5e9e843de770d558834bda
Moustafa Essam <moustafaessam2212@gmail.com>
2025-02-24 21:11:55 +0800

    removing protected routes to help for deployment

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,18 @@
+import ProfileDetailsAndLinksHeader from "@/components/Header/ProfileDetailsAndLinksHeader";
+import CustomizsLinksContainer from "@/components/ProfileDetails/Customize Links/CustomizeLinksContainer/CustomizsLinksContainer";
+import PhoneMainContainer from "@/components/ProfileDetails/Phone/PhoneMainContainer/PhoneMainContainer";
+import ProfileDetailsContainer from "@/components/ProfileDetails/ProfileDetailsContainer/ProfileDetailsContainer";
 import { redirect } from "next/navigation";
 
-export default function Home() {
-  redirect("/login");
+export default function LinksPage() {
+  redirect("/links");
+  return (
+    <>
+      <ProfileDetailsAndLinksHeader />
+      <ProfileDetailsContainer>
+        <PhoneMainContainer />
+        <CustomizsLinksContainer />
+      </ProfileDetailsContainer>
+    </>
+  );
 }
